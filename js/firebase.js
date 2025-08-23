3eee2082761011a139eae03105622e3dbf41ad3d
DOMKA <software.domka@gmail.com>
2025-08-23 00:35:23 -0500

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAzcLdb3lm0efSMuG3U3U8etdb9oJe9SdY",
+  authDomain: "domka-cotizador.firebaseapp.com",
+  projectId: "domka-cotizador",
+  storageBucket: "domka-cotizador.firebasestorage.app",
+  messagingSenderId: "11233894590",
+  appId: "1:11233894590:web:56fb76f5a1ca9af7453eef"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
