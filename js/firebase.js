aba0dc4227b2b3595712650bec4c66a401b4c609
DOMKA <software.domka@gmail.com>
2025-08-23 00:50:53 -0500

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,9 +1,4 @@
-// js/firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
-
-// ⚠️ Reemplaza con tu configuración Firebase
+// firebase.js
 const firebaseConfig = {
   apiKey: "AIzaSyAzcLdb3lm0efSMuG3U3U8etdb9oJe9SdY",
   authDomain: "domka-cotizador.firebaseapp.com",
@@ -13,8 +8,8 @@ const firebaseConfig = {
   appId: "1:11233894590:web:56fb76f5a1ca9af7453eef"
 };
 
-// Inicializa
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+firebase.initializeApp(firebaseConfig);
+window.auth = firebase.auth();
+window.db = firebase.firestore();
+
 
