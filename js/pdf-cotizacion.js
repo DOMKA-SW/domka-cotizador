a260b60789ac9a1b1c80688ac2d195bbf5a48ba2
DOMKA <software.domka@gmail.com>
2025-08-27 15:58:29 -0500

    Update pdf-cotizacion.js

--- js/pdf-cotizacion.js
+++ js/pdf-cotizacion.js
@@ -262,7 +262,7 @@ async function generarPDFCotizacion(cotizacion, nombreCliente = "Cliente") {
       image: images.muneco,
       width: 100,
       opacity: 0.1,
-      absolutePosition: { x: 50, y: 70 }
+      absolutePosition: { x: 20, y: 30 }
     },
       {
         image: images.logo, // Marca de agua centrada
